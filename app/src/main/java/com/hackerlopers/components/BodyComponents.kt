085b8a9fd3e7c1503a11bffb144c6f50fabb91a1
saul-fraire <ext.sfraire@maxillc.com>
2024-10-29 22:57:57 -0600

    HomeView

---
+++ app/src/main/java/com/hackerlopers/components/BodyComponents.kt
@@ -0,0 +1,80 @@
+package com.hackerlopers.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import com.hackerlopers.gamesretrofit.model.GameList
+import com.hackerlopers.gamesretrofit.ui.theme.CustomBlack
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(title: String, showBackButton: Boolean = false, onClickBakButton: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = title, color = Color.White, fontWeight = FontWeight.ExtraBold) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = CustomBlack
+        ),
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = { onClickBakButton() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun CardGame(game: GameList, onClick: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .shadow(40.dp)
+            .clickable {
+                onClick()
+            }
+    ) {
+        Column {
+            MainImage(imagePath = game.backgroundImage)
+        }
+    }
+}
+
+@Composable
+fun MainImage(imagePath: String) {
+    val image = rememberAsyncImagePainter(imagePath)
+    Image(
+        painter = image,
+        contentScale = ContentScale.Crop,
+        contentDescription = "",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    )
+}
--- app/src/main/java/com/hackerlopers/gamesretrofit/views/HomeView.kt
+++ app/src/main/java/com/hackerlopers/gamesretrofit/views/HomeView.kt
@@ -1,5 +1,6 @@
 package com.hackerlopers.gamesretrofit.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,16 +11,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.hackerlopers.components.CardGame
+import com.hackerlopers.components.MainTopBar
+import com.hackerlopers.gamesretrofit.ui.theme.CustomBlack
 import com.hackerlopers.gamesretrofit.viewModel.GamesViewModel
 
 @Composable
-fun HomeView(viewModel: GamesViewModel){
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        val games by viewModel.games.collectAsState()
-        LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            items(games) { item ->
-                Text(text = item.name)
-            }
+fun HomeView(viewModel: GamesViewModel) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MainTopBar(title = "API Games") {}
         }
+    ) { innerPadding ->
+        ContentHomeView(viewModel, Modifier.padding(innerPadding))
+    }
+}
+
+
+@Composable
+fun ContentHomeView(viewModel: GamesViewModel, modifier: Modifier) {
+    val games by viewModel.games.collectAsState()
+    LazyColumn(modifier = modifier
+        .fillMaxSize()
+        .background(color = CustomBlack)) {
+        items(games) { item ->
+            CardGame(game = item, onClick = { })
+            Text(
+                text = item.name,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.White,
+                modifier = Modifier.padding(start = 10.dp)
+            )
+        }
+
     }
 }
